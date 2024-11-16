2b317ab62801f8292ec12e6214d0dc2b8b000561
Livita Touch <150226387+Livita99@users.noreply.github.com>
2024-11-16 19:06:05 +0700

    Add files via upload

---
+++ W5-S2-PRACTICE-Lists-Pass-Functions-Lift-States-Up/EXERCISE-3/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_workspace/W5-S2-Lists-Pass-Functions-Lift-States-Up/EXERCISE-3/screen/welcome.dart';
+import 'package:flutter_workspace/W5-S2-Lists-Pass-Functions-Lift-States-Up/EXERCISE-3/screen/temperature.dart';
+
+class TemperatureApp extends StatefulWidget {
+  const TemperatureApp({super.key});
+
+  @override
+  State<TemperatureApp> createState() {
+    return _TemperatureAppState();
+  }
+}
+
+class _TemperatureAppState extends State<TemperatureApp> {
+  bool _isWelcomeScreen = true;
+
+  void _toggleScreen() {
+    setState(() {
+      _isWelcomeScreen = !_isWelcomeScreen;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xff16C062),
+                Color(0xff00BCDC),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: _isWelcomeScreen
+              ? Welcome(onNavigate: _toggleScreen)
+              : Temperature(onNavigate: _toggleScreen),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(const TemperatureApp());
+}
